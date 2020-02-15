478970551389c582c8d00b93e7ed6c18af604ede
Nhat Quang <thaihuynhatquang@gmail.com>
2020-02-15 19:49:39 +0700

    setup server side

---
+++ server/app/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '../../public')))
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  res.sendStatus(err.status || 500)
+})
+
+export default app
